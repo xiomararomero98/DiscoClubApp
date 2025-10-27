b1d3c71299eb12039e8b2dc7a802ad3421ba5076
Renato Díaz <re.diazp@duocuc.cl>
2025-10-27 19:04:56 -0300

    pequeña correcion

--- app/src/main/java/com/example/discoclub/data/local/pedido/PedidoDao.kt
+++ app/src/main/java/com/example/discoclub/data/local/pedido/PedidoDao.kt
@@ -1,7 +1,6 @@
 package com.example.discoclub.data.local.pedido
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -18,14 +17,12 @@ interface PedidoDao{
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertOne(pedido: PedidoEntity): Long
 
+    @Suppress("unused")
     @Update
     suspend fun updateOne(pedido: PedidoEntity)
 
     @Query("SELECT * FROM Pedido")
     fun getAll(): Flow<List<PedidoEntity>>
-
-    @Query("SELECT * FROM Pedido WHERE id = :id")
-    suspend fun getById(id: Long): PedidoEntity?
 
     @Query("DELETE FROM Pedido WHERE id = :id")
     suspend fun deleteById(id: Long): Int
